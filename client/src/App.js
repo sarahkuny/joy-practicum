a5588cdaf61dfa50776535b6eb308bed03bb5a92
JoyAnneW <joyawilliams@gmail.com>
2022-05-05 20:16:10 +0200

    installed tailwind

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,11 @@
-import logo from "./logo.svg";
 import "./App.css";
 
 function App() {
-	return <div className="App"></div>;
+	return (
+		<div className="App">
+			<h1 className="text-3xl font-bold underline">PRACTICUM</h1>
+		</div>
+	);
 }
 
 export default App;
