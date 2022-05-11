cd73ea30cf74cbeda33ca44050c20183a32d711a
JoyAnneW <joyawilliams@gmail.com>
2022-05-11 12:59:35 +0200

    implemented put route in students to handle changes to project_id and instructor_id

--- routes/students.js
+++ routes/students.js
@@ -66,17 +66,31 @@ router.delete("/:id", async (req, res, next) => {
 // PUT: edit student
 router.put("/:id", async (req, res, next) => {
 	const { id } = req.params;
+	const { project_id, instructor_id } = req.body;
+	console.log(req.body);
+
 	try {
+		// find the specific project
 		let results = await db(
-			`SELECT FROM bootcamp_instructors WHERE student_id=${id};`
+			`SELECT * FROM bootcamp_students WHERE student_id=${id}`
 		);
-		results = await db("SELECT * FROM bootcamp_instructors");
+		// if it is found, create and use the sql instructions for updating this item
 		if (results.data.length) {
-			res.status(200).send(results.data);
-		} else {
-			res.status(404).send({ error: "Resource not found" });
+			const sql = `UPDATE bootcamp_students SET project_id="${project_id}", instructor_id="${instructor_id}" WHERE student_id=${id};`;
+
+			// this replaces the specified item
+			await db(sql);
+
+			// now results is updated to be what's returned from fetching the entire table.
+			results = await db("SELECT * FROM bootcamp_students");
+			if (results.data.length) {
+				res.status(200).send(results.data);
+			} else {
+				res.status(404).send({ error: "Resource not found" });
+			}
 		}
 	} catch (err) {
+		console.log(err);
 		res.status(500).send({ Error: err.message });
 	}
 });
