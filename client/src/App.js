47a9e7fb16efe3c353c4f805deb5e8a1903be0b1
JoyAnneW <joyawilliams@gmail.com>
2022-05-14 19:20:25 +0200

    rendered table with data from joined sql query in FilteredList component

--- client/src/App.js
+++ client/src/App.js
@@ -18,12 +18,22 @@ function App() {
 			.catch((error) => console.error(error));
 	}, []);
 
+	const toggleShowList = () => {
+		setShowList(!showList);
+	};
+
 	return (
 		<div className="App">
 			<h1 className="text-3xl font-bold underline">PRACTICUM</h1>
 			<ContactForm />
 			<ProjectsForm projects={projects} setProjects={setProjects} />
 			<Projects projects={projects} setProjects={setProjects} />
+			<button
+				onClick={toggleShowList}
+				className="block bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+			>
+				Show All Assignments
+			</button>
 			<FilteredList />
 		</div>
 	);
--- client/src/components/FilteredList.js
+++ client/src/components/FilteredList.js
@@ -15,20 +15,26 @@ export default function FilteredList({ instructors }) {
 
 	return (
 		<div>
-			<div className="block p-6 border border-red-300 max-w-sm bg-white rounded-lg  shadow-md hover:bg-gray-100">
-				{filteredList.map((item) => {
-					return (
-						<div>
-							{item.instructor_name}
-							{item.first_name}
-							{item.last_name}
-						</div>
-					);
-				})}
+			<div className=" p-6 border border-red-300 max-w-sm bg-white rounded-lg  shadow-md hover:bg-gray-100">
+				<table>
+					<tr>
+						<th>Intructors</th>
+						<th>Students</th>
+						<th>Project ID</th>
+					</tr>
+					{filteredList.map((item) => {
+						return (
+							<tr>
+								<td>{item.instructor_name}</td>
+								<td>
+									{item.first_name} {item.last_name}
+								</td>
+								<td>{item.project_id}</td>
+							</tr>
+						);
+					})}
+				</table>
 			</div>
-			<button className="block bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
-				Show
-			</button>
 		</div>
 	);
 }
