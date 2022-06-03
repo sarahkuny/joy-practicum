93de171713d8a81efa789af7ee6c0287e31706c6
Sarah Kuny <sarahkuny@icloud.com>
2022-06-02 19:32:30 -0500

    refactor handleAddPerson to include auth header

--- client/src/components/NewPersonForm.js
+++ client/src/components/NewPersonForm.js
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-
+import axios from "axios";
 // since instructors and students have the same data structure and require the same functions and form, I made this component reusable. The major functionality is determined by what's passed in props. stateSetter is either the setStudents or setInstructors function which is called after the api gives us a response. The enpoint of the fetch route is determined by the value of the route prop (either "students" or "instructors")
 export default function NewPersonForm({ stateSetter, route }) {
 	const [details, setDetails] = useState({
@@ -20,31 +20,51 @@ export default function NewPersonForm({ stateSetter, route }) {
 
 	console.log(details);
 
-	const handleAddPerson = (event) => {
+	const handleAddPerson = async (event) => {
 		event.preventDefault();
-
-		fetch(`/api/${route}`, {
-			method: "POST",
-			headers: {
-				"Content-Type": "application/json",
-			},
-			body: JSON.stringify(details),
-		})
-			.then((response) => response.json())
-			.then((data) => {
-				// in oder to dynamically determine the name of the function that will be called here (either setStudents or setInstructors), I need to first pass the function (from the stateSetter prop) as the value of a variable with a different name. Then call the stateSetter function using that variable name.
-				const setState = stateSetter;
-				console.log(data);
-				setState(data);
-			})
-			.catch((error) => console.error(error));
-
+		let token = localStorage.getItem("token");
+		try {
+			const { data } = await axios("/api/${route}", {
+				method: "POST",
+				headers: {
+					authorization: `Bearer ${token}`
+				},
+			});
+			const setState = stateSetter;
+			setState(data);
+		}
+		catch(err) {
+			console.error(err)
+		}
 		setDetails({
 			first_name: "",
 			last_name: "",
 			email: "",
 		});
-	};
+		};
+		
+		// fetch(`/api/${route}`, {
+		// 	method: "POST",
+		// 	headers: {
+		// 		"Content-Type": "application/json",
+		// 	},
+		// 	body: JSON.stringify(details),
+		// })
+		// 	.then((response) => response.json())
+		// 	.then((data) => {
+		// 		// in oder to dynamically determine the name of the function that will be called here (either setStudents or setInstructors), I need to first pass the function (from the stateSetter prop) as the value of a variable with a different name. Then call the stateSetter function using that variable name.
+		// 		const setState = stateSetter;
+		// 		console.log(data);
+		// 		setState(data);
+		// 	})
+		// 	.catch((error) => console.error(error));
+
+		// setDetails({
+		// 	first_name: "",
+		// 	last_name: "",
+		// 	email: "",
+		// });
+	//};
 	return (
 		<div className="w-96 z-10 p-6 border border-indigo-300 max-w-lg bg-white rounded-lg  shadow-md  mt-4">
 			<form
