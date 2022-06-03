5dcbdac86fc66d1072c8c8995bd4aaa36f76d2ae
Sarah Kuny <sarahkuny@icloud.com>
2022-06-02 19:08:12 -0500

    refactor useEffect methods to include auth header

--- client/src/components/StaffView.js
+++ client/src/components/StaffView.js
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from "react";
 import Projects from "./Projects";
 import Sidebar from "./Sidebar";
+import axios from "axios";
 
 export default function StaffView({
 	projects,
@@ -11,23 +12,67 @@ export default function StaffView({
 	const [students, setStudents] = useState([]);
 	const [instructors, setInstructors] = useState([]);
 
-	useEffect(() => {
-		fetch("/api/students/")
-			.then((response) => response.json())
-			.then((data) => {
-				console.log(data);
-				setStudents(data);
-			})
-			.catch((error) => console.error(error));
 
-		fetch("/api/instructors/")
-			.then((response) => response.json())
-			.then((data) => {
-				console.log(data);
-				setInstructors(data);
-			})
-			.catch((error) => console.error(error));
-	}, []);
+	useEffect( () => {
+		const fetchStudents = async (token) => {
+			try {
+				const { data } = await axios("/api/students", {
+					headers: {
+						authorization: `Bearer ${token}`
+					},
+				});
+				setStudents(data)
+			}
+			catch(err) {
+				console.error(err)
+			}
+		};
+		const fetchInstructors = async (token) => {
+			try {
+				const { data } = await axios("/api/instructors", {
+					headers: {
+						authorization: `Bearer ${token}`
+					},
+				});
+				setInstructors(data)
+			}
+			catch(err) {
+				console.error(err)
+			}
+		}
+
+		let token = localStorage.getItem("token");
+		fetchStudents(token);
+		fetchInstructors(token);
+		},[]);
+
+	// useEffect(() => {
+	// 	fetch("/api/students/",
+	// 	{
+	// 		headers: {
+	// 			authorization: "Bearer" + localStorage.getItem("token")
+	// 		}
+	// 	})
+	// 		.then((response) => response.json())
+	// 		.then((data) => {
+	// 			console.log(data);
+	// 			setStudents(data);
+	// 		})
+	// 		.catch((error) => console.error(error));
+
+	// 	fetch("/api/instructors/",
+	// 	{
+	// 		headers: {
+	// 			authorization: "Bearer" + localStorage.getItem("token")
+	// 		}
+	// 	})
+	// 		.then((response) => response.json())
+	// 		.then((data) => {
+	// 			console.log(data);
+	// 			setInstructors(data);
+	// 		})
+	// 		.catch((error) => console.error(error));
+	// }, []);
 
 	return (
 		<div className="flex gap-4">
